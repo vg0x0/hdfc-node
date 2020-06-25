88f54af8bbf43e89de762006ac8138511a8f2ea8
vg0x0 <vikash.gupta@in.ibm.com>
2020-06-25 08:04:40 -0500

    third commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody Demo for HDFC Bank!");
+  res.send("Hello from Cloud Pak Appsody Demo for HDFC Bank!");
 });
  
 module.exports.app = app;
